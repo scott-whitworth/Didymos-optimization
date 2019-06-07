12362f750c6f5e938e6eb0c5c36fcda19b51580a
Lauren A Taylor <ltaylor20@my.whitworth.edu>
2019-06-07 15:59:02 -0700

    it merged weird, so we fixed it

--- development/source/orbitalMotion.cpp
+++ development/source/orbitalMotion.cpp
@@ -18,68 +18,40 @@ int main()
     y0.vtheta= 16716.9055348804/AU;// azimuthal velocity (rad/s)
     y0.vz= -81.4453413932308/AU;// off-plane velocity (au/s)
 
-    // conditions for the acceleration components
-    double tau = 3./4;
-    double gamma = 3./4;
-    double accel = 0.0001/AU;
 
-double accel = 0.0001/AU;// thrust acceleration (au/s^2)
+    double accel = 0.0001/AU;// thrust acceleration (au/s^2)
 
-// setup of thrust angle calculations
-coefficients<double> coeff;
-for (int i : coeff.gamma){
-  coeff.gamma[i]=1;
-}
-for (int i : coeff.tau){
-  coeff.tau[i]=1;
-}
 
-    // seting Runge-Kutta tolerance
+    // setting time parameters
+    double timeInitial=0; 
+    double timeFinal=6.653820100923719e+07; // Orbital period of asteroid(s)
+    double deltaT; // time step
+    int numSteps = 500; // initial guess for the number of time steps
+    deltaT = (timeFinal-timeInitial)/1e9; // initial guess for time step, small is preferable
+
+    // setup of thrust angle calculations
+    coefficients<double> coeff;
+    for (int i : coeff.gamma){
+      coeff.gamma[i]=1;
+    }
+    for (int i : coeff.tau){
+      coeff.tau[i]=1;
+    }
+
+    // setting Runge-Kutta tolerance
     double absTol = 1e-9;
 
     // Initialize memory for the solution vector of the dependant solution
     //TODO: SC: That is *times? Where and why are you using numSteps == 500?
-    elements<double>* y;
-    y = new elements<double>[numSteps];
+    elements<double>* yp;
+    yp = new elements<double>[numSteps];
     double *times;
     times = new double[numSteps];
 
-    // Recording the start time for performance metric
-    std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
-
-    // Our output function (yp)
-    //TODO: SC: This is not a helpful comment, 'Our' is nebulous, output function (yp) does not describe what an output function (yp) is 
-    elements<double> *yp;
-    for (int repeat = 0; repeat<1; repeat++){ //TODO: SC: I know why this was put in but it should either: 1) be made part of the code or 2) removed
-        //TODO: SC: While I would not normally say a function call needs a comment, this function call is complicated. A comment or two about what is going on here is needed. A list a veriables is not inherently readable
-        yp = rk4sys(timeInitial,timeFinal,times,y0,deltaT,y,absTol,accel,tau,gamma);
+  
+    for (int repeat = 0; repeat<1; repeat++){
+      yp = rk4sys(timeInitial,timeFinal,times,y0,deltaT,yp,absTol,coeff,accel);
     }
-    // recording stop time
-    std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
-
-    // calculating elapsed time of rk4sys() call(s)
-    std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
-    std::cout << "rk4sys() call took " << time_span.count() << " seconds." << std::endl;
-
-//    Our output function (yp)
-elements<double> *yp;
-for (int repeat = 0; repeat<1; repeat++){
-  yp = rk4sys(timeInitial,timeFinal,times,y0,deltaT,y,absTol,coeff,accel);
-}
-//    recording stop time
-  std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
-    //TODO: SC: Why?
-    if (time_span.count()==0)
-        std::cout<<"I am speed" << std::endl;
-
-    // Output of yp to a text file
-    std::ofstream output;    
-    output.open ("orbitalMotion-accel.bin", std::ios::binary);
-
-//    Output of elasped time
-  std::cout << "It took me " << time_span.count() << " seconds." << std::endl;
-if (time_span.count()==0)
-std::cout<<"I am speed" << std::endl;
 
 // Output of yp to a binary file
   std::ofstream output;
@@ -96,6 +68,7 @@ std::cout<<"I am speed" << std::endl;
     // cleaning up dynamic yp
     //TODO: SC: What about times? Might want to clean that up too
     delete [] yp;
+    delete [] times;
 
     return 0;
 }
