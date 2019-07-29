a10be7fe9bd4155656b645c09c569dc4147e225f
Lauren A Taylor <username@users.noreply.github.com>
2019-07-29 14:00:47 -0700

    updated comments for Earth_calculations folder in Cuda

--- Cuda/headers/Earth_calculations/earthInfo.cpp
+++ Cuda/headers/Earth_calculations/earthInfo.cpp
@@ -4,47 +4,70 @@
 #include "earthInfo.h"
 #include "orbitalMotion.h"
     
-#include <iostream>  
+#include <iostream>  // cout
 #include <iomanip> // setprecision(int)  
 using namespace std;
 
+
 EarthInfo::EarthInfo(const double & beginTime, const double & stopTime, const double & timeAcc)
 {
-    // Setting up initial information
+    ////////////////////////////////////
+    ///Setting up initial information///
+    ////////////////////////////////////
+
     startTime = beginTime; // Starting time (s)
     endTime = stopTime; // Ending time (s)
     timeRes = timeAcc; // Time resolution (s)
     tolData = ((endTime-startTime)/timeRes) + 1; // Total Number of Data points (in hours) in earthCon, plus one for the last 'section'
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     // Alocate memory for earthCon, one entry for every data point
     earthCon = new elements<double> [tolData];
 
+    // Assigning the position of the earth at impact to variable earth. Passed into earthInitial_incremental and rk4Reverse.
     elements<double> earth = elements<double>(R_FIN_EARTH, THETA_FIN_EARTH, Z_FIN_EARTH, VR_FIN_EARTH, VTHETA_FIN_EARTH, VZ_FIN_EARTH);
-    // This one is at triptime 0 or at the startTime
-    earthCon[0]=earthInitial_incremental(startTime,startTime,earth);// Obtaining conditions of the earth every hour
-    std::cout << std::endl;
-    std::cout << "calculating earth positions for the trip time range" << std::endl;
+
+    // Setting the first element of earthCon to be equal to the earth conditions at impact
+    earthCon[0]=earthInitial_incremental(startTime,startTime,earth);
+
+    // Shows progress of earth position calculations before the optimization in cuda can occur.
+    std::cout << std::endl << "calculating earth positions for the trip time range" << std::endl;
     std::cout << "          10 20 30 40 50 60 70 80 90 100" << std::endl;
     std::cout << "progress:[";
+
     for(int i=1; i<tolData; i++)
     { 
+        // Calculates earth's condition at each point (time) from the previously calculated point.
         earthCon[i]=earthInitial_incremental(calc_time(i)-timeRes,calc_time(i),earth);// Obtaining conditions of the earth
+
+        // Filling progress bar
         if((i % (tolData/10)) == 0){
         std::cout << ">>>";
         }
+
+        // Sets earth equal to the conditions calculated for a given time.
         earth=earthCon[i];
     }
+    // Closing progress bar
     std::cout << "]" << std::endl << std::endl;
 }
 
+
 elements<double> EarthInfo::getCondition(const double & currentTime)
 {
+    // Defining variables //
     elements<double> lower;
     elements<double> upper;
     int index;
-    index = calcIndex(currentTime); // Location of data (in hours) based on time (in seconds)
+
+    // Setting index equal to the location of data based on time
+    index = calcIndex(currentTime); 
+    // The lower index weight is equal to the index
     lower = earthCon[index];
+    // The upper index weight is equal to the index plus 1
     upper = earthCon[index + 1];
+    
+    // Weights are used for interpolation
     double lowerWeight = 1 - ((currentTime-calc_time(index))/timeRes);
     double upperWeight = 1 - ((calc_time(index+1)-currentTime)/timeRes);
 
--- Cuda/headers/Earth_calculations/orbitalMotion.cpp
+++ Cuda/headers/Earth_calculations/orbitalMotion.cpp
@@ -8,18 +8,17 @@
 
 elements<double> earthInitial_incremental(double timeInitial, double tripTime,const elements<double> & earth)
 {
+  // Time step
+  double deltaT; 
 
-  //setting initial conditions for calculation of earth on launch date with orbital elements of the earth on the asteroid impact date of 2022-10-05.
-  // setting intiial time parameters
-  double deltaT; // time step
-  deltaT = -(tripTime - timeInitial)/60; // initial guess for time step, small is preferable
-  // declaring the solution vector
+  // Initial guess for time step, cannot be greater than the time resolution.
+  deltaT = -(tripTime - timeInitial)/60; 
+
+  // Declaring the solution vector.
   elements<double> yp;
-  // setting Runge-Kutta tolerance
-  double absTol = RK_TOL;
 
-  // calculates the earth's launch date conditions based on timeFinal minus the optimized trip time
-  rk4Reverse(timeInitial,tripTime,earth,deltaT,yp,absTol);
+  // Calculates the earth's launch date conditions based on timeFinal minus the optimized trip time.
+  rk4Reverse(timeInitial,tripTime,earth,deltaT,yp,RK_TOL);
  
   return yp;
 }
