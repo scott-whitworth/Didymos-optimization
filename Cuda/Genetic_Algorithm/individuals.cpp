80d54870e33b27bdadaaabcc018979e1b608cd82
Matthew Lochridge <mdlochridge@gmail.com>
2020-06-19 15:48:41 -0700

    maybe?

--- Cuda/Genetic_Algorithm/individuals.cpp
+++ Cuda/Genetic_Algorithm/individuals.cpp
@@ -11,30 +11,24 @@ double Individual::getCost(double & ratio) {
 }
 
 bool Individual::operator>(Individual &other) {
-    if (this->cost > other.cost {
+    if (this->cost > other.cost) {
         return true;
     }
-    else {
-        return false;
-    }
+    else return false;
 }
 
 bool Individual::operator<(Individual &other) {
-    if (this->cost < other.cost {
+    if (this->cost < other.cost) {
         return true;
     }
-    else {
-        return false;
-    }
+    else return false;
 }
 
 bool Individual::operator==(Individual &other) {
     if (this->cost == other.cost) {
         return true;
     }
-    else {
-        return false;
-    }
+    else return false;
 }
 
 /*
@@ -50,11 +44,11 @@ bool betterInd(Individual first, Individual second) {
 */
 
 double getPosRatio(geneticConstants & gConfig) {
-    if (posDiff > gConfig.pos_threshold) {
+    if (this->posDiff > gConfig.pos_threshold) {
         return 1.0; // focus entirely on position because the spacecraft is very far from the asteroid
     }
     else {
-        return posDiff / gConfig.pos_threshold; // focus more on position the greater the difference is based on linear scale
+        return this->posDiff / gConfig.pos_threshold; // focus more on position the greater the difference is based on linear scale
     }
 }
 
