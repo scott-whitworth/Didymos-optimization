7a70b6e54c4042eceb684559ad7a576dd3f473b1
Matthew Lochridge <mdlochridge@gmail.com>
2020-06-21 18:00:51 -0700

    try

--- Cuda/Genetic_Algorithm/individuals.cpp
+++ Cuda/Genetic_Algorithm/individuals.cpp
@@ -12,13 +12,12 @@ double Individual::getCost() {
 }
 
 double Individual::getPosRatio() {
-    double pos_diff = this->posDiff;
     double ratio;
-    if (pos_diff > POS_THRESH) {
+    if (this->posDiff > POS_THRESH) {
         ratio = 1.0; // focus entirely on position because the spacecraft is very far from the asteroid
     }
     else {
-        ratio = pos_diff / POS_THRESH; // focus more on position the greater the difference is based on linear scale
+        ratio = this->posDiff / POS_THRESH; // focus more on position the greater the difference is based on linear scale
     }
     return ratio;
 }
