827c8bd5a5d622c815a622f9ef751465f19fbed3
Matthew Lochridge <mdlochridge@gmail.com>
2020-06-24 10:42:22 -0700

    changed output pull

--- Cuda/Config_Constants/config.cpp
+++ Cuda/Config_Constants/config.cpp
@@ -149,7 +149,7 @@ void cudaConstants::geneticFileRead(std::string fileName) {
                 }
                 else if (variableName == "vr_fin_earth") {
                     this->vr_fin_earth = std::stod(variableValue);
-                    std::cout << variableName << ": " << variableValue << std::endl;
+                    std::cout << variableName << ": " << this->vr_fin_earth << std::endl;
                 }
                 else if (variableName == "vtheta_fin_earth") {
                     this->vtheta_fin_earth = std::stod(variableValue);
