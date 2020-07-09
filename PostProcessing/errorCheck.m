37d1a635bb893c36c3243605cfd42771864ab3fc
Matthew Lochridge <mdlochridge@gmail.com>
2020-07-09 15:28:09 -0700

    fixed error plots

--- PostProcessing/errorCheck.m
+++ PostProcessing/errorCheck.m
@@ -8,7 +8,17 @@ function [] = errorCheck(seed)
     W = A(2,:);
     dE = A(3,:);
 
+    dW = zeros(1,size(W,2));
+    dE = zeros(1,size(E,2));
+    err = zeros(1,size(t,2));
+    
+    for i = 2:size(t,2)
+        dW(i) = W(i) - W(i-1);
+        dE(i) = E(i) - E(i-1);
+        err(i) = (dW(i) - dE(i))./E(i);
+    end
+    
     figure(1)
-    plot(t, (W-dE)./dE)
-    xlabel('t (s)')
+    plot(t, err)
+    xlabel('t')
     ylabel('% error')
