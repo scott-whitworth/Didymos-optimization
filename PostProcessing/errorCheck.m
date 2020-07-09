32fe5cd4db16805f705b57b3c48d6858602dade3
Matthew Lochridge <mdlochridge@gmail.com>
2020-07-09 14:34:37 -0700

    errorCheck.m

---
+++ PostProcessing/errorCheck.m
@@ -0,0 +1,15 @@
+function [] = errorCheck(seed)
+
+    filename = join(['errorCheck-',num2str(seed),'.bin']);
+    file = fopen(filename);
+    A = fread(file,[3,Inf],'double');
+    
+    t = A(1,:);
+    W = A(2,:);
+    dE = A(3,:);
+    
+    figure(1)
+    plot(t, W-dE)
+    title('Error')
+    xlabel('t')
+    ylabel('W - \DeltaE')
