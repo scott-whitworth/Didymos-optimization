5fb4184e05609c4d5f27e107522e4058ac88270c
Matthew Lochridge <mdlochridge@gmail.com>
2020-07-09 20:45:07 -0700

    fixde error calculation

--- PostProcessing/errorCheck.m
+++ PostProcessing/errorCheck.m
@@ -11,14 +11,19 @@ function [] = errorCheck(seed)
     dW = zeros(1,size(W,2));
     dE = zeros(1,size(E,2));
     err = zeros(1,size(t,2));
-    
+
     for i = 2:size(t,2)
-        dW(i) = W(i) - W(i-1);
+        dW(i) = W(i-1) * (t(i)-t(i-1))/t(i-1);
         dE(i) = E(i) - E(i-1);
         err(i) = (dW(i) - dE(i))./E(i);
     end
     
     figure(1)
-    plot(t, err)
-    xlabel('t')
-    ylabel('% error')
+    plot(t, err * 100)
+    xlabel('t'), ylabel('% error')
+    xlim([0, t(end)])
+    
+    % figure(2)
+    % plot(t, dW - dE)
+    % xlabel('t'), ylabel('\DeltaW - \DeltaE')
+    % xlim([0, t(end)])
