423d3719c8ed36057493f0b8a2138c22e2f27ad0
Matthew Lochridge <mdlochridge@gmail.com>
2020-07-10 10:42:12 -0700

    fixed error plot

--- PostProcessing/errorCheck.m
+++ PostProcessing/errorCheck.m
@@ -3,27 +3,35 @@ function [] = errorCheck(seed)
     filename = join(['errorCheck-',num2str(seed),'.bin']);
     file = fopen(filename);
     A = fread(file,[3,Inf],'double');
-    
+
     t = A(1,:);
     W = A(2,:);
     dE = A(3,:);
-
-    dW = zeros(1,size(W,2));
-    dE = zeros(1,size(E,2));
-    err = zeros(1,size(t,2));
-
-    for i = 2:size(t,2)
-        dW(i) = W(i-1) * (t(i)-t(i-1))/t(i-1);
-        dE(i) = E(i) - E(i-1);
-        err(i) = (dW(i) - dE(i))./E(i);
-    end
+    Etot = A(4,:);
+    err = (W-dE)./Etot;
     
     figure(1)
-    plot(t, err * 100)
+    plot(t, err * 100, '.')
     xlabel('t'), ylabel('% error')
     xlim([0, t(end)])
     
+    % Using old work calculation in output.cpp:
+    
+    % dW = zeros(1,size(W,2));
+    % dE = zeros(1,size(E,2));
+    % err = zeros(1,size(t,2));
+    
+    % for i = 2:size(t,2)
+    %     dW(i) = W(i-1) * (t(i)-t(i-1))/t(i-1);
+    %     dE(i) = E(i) - E(i-1);
+    %     err(i) = (dW(i) - dE(i))./E(i);
+    % end
+    
+    % err = err * 100;
+    
+    % To check error calculation:
+    
     % figure(2)
-    % plot(t, dW - dE)
-    % xlabel('t'), ylabel('\DeltaW - \DeltaE')
+    % plot(t, W - dE)
+    % xlabel('t'), ylabel('W - \DeltaE')
     % xlim([0, t(end)])
