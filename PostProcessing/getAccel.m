79bd3bee1abe252681b126afd08d2e968db8ef3d
Matthew Lochridge <mdlochridge@gmail.com>
2020-07-14 10:27:13 -0700

    1

--- PostProcessing/getAccel.m
+++ PostProcessing/getAccel.m
@@ -1,6 +1,6 @@
 function [accelX,accelY,accelZ] = getAccel(cR,tripTime,gammaCoeff,tauCoeff,sizeC)
     [gamma,tau]=angles(cR(7,1:sizeC),tripTime,gammaCoeff,tauCoeff);
-    vpa(gamma), vpa(tau)
+    vpa(gamma(1)), vpa(tau(1))
     accelR = cR(10,1:sizeC).*cos(tau).*sin(gamma);
     accelTheta = cR(10,1:sizeC).*cos(tau).*cos(gamma);
     accelZ = cR(10,1:sizeC).*sin(tau);
