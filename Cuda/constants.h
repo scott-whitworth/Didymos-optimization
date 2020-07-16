0be1c43d5b1a169a2f2b2382dafe4ee7abf99722
Matthew Lochridge <mdlochridge@gmail.com>
2020-07-16 11:30:09 -0700

    note

--- Cuda/constants.h
+++ Cuda/constants.h
@@ -17,6 +17,7 @@
 
 
 // Starting location and sizes in the optimization array for navigation to access specific values
+// Note: array sizes must be odd, coinciding with their use in computing Fourier series
 // WARNING: As of July 6th, the file used in non-random start is based on previous definition of values and so if these are changed from old sizes then it may cause issues for it
 #define GAMMA_ARRAY_SIZE 7  // Number of coefficients for gamma
 #define   TAU_ARRAY_SIZE 3  // Number of coefficients for tau
