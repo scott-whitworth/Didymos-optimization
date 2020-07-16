a413e6f1be46755c8b1a9b81c3a03e546b134d37
Andrew O'Kins <aokins22@whitworth.edu>
2020-07-16 14:04:01 -0700

    Created recordMutateFile function, adjusted double_mutate_rate property > Functionalized outputting to mutateFile, added number of genes being mutated as a column > recordMutateFile is called both in mutate() and also in the case of no mutation occurring, requires record_mode to be equal to true > double_mutate_rate now refers to percentage of mutations being exactly 2 genes (not 2 or 3 total)

--- Cuda/output.cpp
+++ Cuda/output.cpp
@@ -21,7 +21,7 @@ void setMutateFile(const cudaConstants* cConstants) {
   std::ofstream mutateFile;
   mutateFile.open("mutateFile-" + std::to_string(cConstants->time_seed) + ".csv", std::ios_base::app);
 
-  mutateFile << "gen, anneal,";
+  mutateFile << "gen, anneal, genesToMutate,";
   for (int i = 0; i < GAMMA_ARRAY_SIZE; i++) {
     mutateFile << "gamma" << i << ",";
   }
@@ -33,6 +33,38 @@ void setMutateFile(const cudaConstants* cConstants) {
   }
   mutateFile << "alpha,beta,zeta,tripTime\n";
 
+  mutateFile.close();
+}
+
+// input: cConstants - access time_seed to derive file name
+//        generation - for storing into the file, the generation that the mutation is occuring
+//        annealing - for storing into the file, the anneal value being used
+//        numGenes - the number of genes that are to be mutated
+//        recordLog[OPTIM_VARS] - a "mutate mask" that is an array corresponding to the optimization array that contains the random mutate values being applied
+// output: file mutateFile-[time_seed].csv is appended a new row containing mutate information
+void recordMutateFile(const cudaConstants * cConstants, double generation, double annealing, int numGenes, double recordLog[OPTIM_VARS]) {
+  std::ofstream mutateFile;
+  mutateFile.open("mutateFile-" + std::to_string(cConstants->time_seed) + ".csv", std::ios_base::app);
+
+  // Record generation, annealing, and number of genes that should be impacted
+  mutateFile << generation << "," << annealing << "," << numGenes << ",";
+
+  // Record gamma mutation values
+  for (int i = GAMMA_OFFSET; i < (GAMMA_OFFSET + GAMMA_ARRAY_SIZE); i++) {
+      mutateFile << recordLog[i] << ",";
+  }
+  // Record tau mutation values
+  for (int i = TAU_OFFSET; i < (TAU_OFFSET + TAU_ARRAY_SIZE); i++) {
+      mutateFile << recordLog[i] << ",";
+  }
+  // Record coast mutation values
+  for (int i = COAST_OFFSET; i < (COAST_OFFSET + COAST_ARRAY_SIZE); i++) {
+      mutateFile << recordLog[i] << ",";
+  }
+  // Record alpha, beta, zeta, tripTime
+  mutateFile << recordLog[ALPHA_OFFSET] << "," << recordLog[BETA_OFFSET] << "," << recordLog[ZETA_OFFSET] << "," << recordLog[TRIPTIME_OFFSET] << ",";
+  mutateFile << "\n";
+  
   mutateFile.close();
 }
 
